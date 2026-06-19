4e11284c39778ed1317062358d071508425ff298
Roman Parkhomenko <roman.s.parkhomenko@gmail.com>
2026-06-19 15:59:50 +0300

    vid#17

--- app/(dashboard)/dashboard/users/[id]/page.tsx
+++ app/(dashboard)/dashboard/users/[id]/page.tsx
@@ -1,5 +1,5 @@
-export default function User({ params }: { params: { id: string } }) {
-  const { id } = params;
+export default async function User({ params }: { params: { id: string } }) {
+  const { id } = await params;
   return (
     <h1 className="text-3xl font-bold underline text-center">
       User {id} details
--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,5 +1,16 @@
-export default function About() {
-  // throw new Error('Something went wrong');
-
-  return <h1>About</h1>;
+export default async function About() {
+  const response = await fetch('http://jsonplaceholder.typicode.com/albums');
+  if (!response.ok) throw new Error('Failed to fetch response');
+  const list = await response.json();
+  const tenItems = list.slice(0, 10);
+  return (
+    <>
+      <h1>About</h1>
+      {tenItems.map(({ id, title }: { id: number; title: string }) => (
+        <p key={id}>
+          {id}: <span>{title}</span>
+        </p>
+      ))}
+    </>
+  );
 }
