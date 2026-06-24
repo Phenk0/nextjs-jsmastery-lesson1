2cc4ba5c0b1e2cc8faef96de3c53bbefd79677ce
Roman Parkhomenko <roman.s.parkhomenko@gmail.com>
2026-06-24 11:22:01 +0300

    implemented fonts & inserted assets; TODO: vid->#23

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,10 @@ import Hello from '@/app/Components/Hello';
 export default function Home() {
   return (
     <>
-      <h1 className="h1-bold">Welcome to Next.js 15</h1>
+      <h1 className="font-space-grotesk h1-bold text-light-500">
+        Welcome to Next.js 15
+      </h1>
+      <h1 className="font-inter h1-bold">Welcome to Next.js 15</h1>
       <Hello />
     </>
   );
